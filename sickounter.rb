ed78ea138fcb3781431b92ffc6cddc2c43cdd9d9
Marcin Kulik <marcin.kulik@gmail.com>
2009-03-21 01:59:59 +0100

    checking for existence of f param

--- sickounter.rb
+++ sickounter.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  url = params[:f]
+  url = params[:f] or raise Sinatra::NotFound
   filename = url.gsub(/https?:\/\//, "").gsub(/[^a-zA-Z0-9_-]/, ".") + ".txt"
   path = "counters/#{filename}"
   val = (File.read(path).strip.to_i rescue 0) + 1
