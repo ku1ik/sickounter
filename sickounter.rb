1c62f6faca07503dd6a3fa6a28fb8a1c5501e8f4
Marcin Kulik <marcin.kulik@gmail.com>
2009-03-21 01:12:01 +0100

    first commit

---
+++ sickounter.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  url = params[:f]
+  filename = url.gsub(/https?:\/\//, "").gsub(/[^a-zA-Z0-9_-]/, ".") + ".txt"
+  path = "counters/#{filename}"
+  val = (File.read(path).strip.to_i rescue 0) + 1
+  puts val
+  File.open(path, "w") do |f|
+    f.flock(File::LOCK_EX)
+    f.puts(val.to_s)
+    f.flock(File::LOCK_UN)
+  end
+  redirect(url)
+end
+
