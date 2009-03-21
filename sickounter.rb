d55bde6f13cf98d7a12f5df57a2e58d40026e3ff
Marcin Kulik <marcin.kulik@gmail.com>
2009-03-21 10:55:10 +0100

    removed puts

--- sickounter.rb
+++ sickounter.rb
@@ -6,7 +6,6 @@ get '/' do
   filename = url.gsub(/https?:\/\//, "").gsub(/[^a-zA-Z0-9_-]/, ".") + ".txt"
   path = "counters/#{filename}"
   val = (File.read(path).strip.to_i rescue 0) + 1
-  puts val
   File.open(path, "w") do |f|
     f.flock(File::LOCK_EX)
     f.puts(val.to_s)
