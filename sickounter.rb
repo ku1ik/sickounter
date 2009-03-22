29b887cbcedf2f86682eead0f2c5c8bcdba6a71f
Marcin Kulik <marcin.kulik@gmail.com>
2009-03-22 01:46:39 +0100

    getting counters dir relative __FILE__

--- sickounter.rb
+++ sickounter.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 
+COUNTERS_DIR = File.join(File.dirname(__FILE__), 'counters')
+
 get '/' do
   url = params[:f] or raise Sinatra::NotFound
   filename = url.gsub(/https?:\/\//, "").gsub(/[^a-zA-Z0-9_-]/, ".") + ".txt"
-  path = "counters/#{filename}"
+  path = File.join(COUNTERS_DIR, filename)
+  puts path
   val = (File.read(path).strip.to_i rescue 0) + 1
   File.open(path, "w") do |f|
     f.flock(File::LOCK_EX)
@@ -16,8 +19,8 @@ end
 
 get '/stats' do
   @downloads = {}
-  Dir["counters/*.txt"].each do |counter|
-    @downloads[counter.split("/")[1]] = File.read(counter).strip.to_i
+  Dir[File.join(COUNTERS_DIR, "*.txt")].each do |counter|
+    @downloads[counter.split("/")[-1]] = File.read(counter).strip.to_i
   end
   erb :stats
 end
